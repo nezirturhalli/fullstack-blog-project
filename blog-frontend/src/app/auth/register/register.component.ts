011d7eda1e5162dfdbcaf75b7668f638f99cfc47
Nezir Turhallı <nzr.trhll@gmail.com>
2022-08-10 13:25:49 +0300

    initial commit

--- blog-frontend/src/app/auth/register/register.component.ts
+++ blog-frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Register } from '../register';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  register : Register;
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router:Router) {
+    this.registerForm = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    });
+    this.register = {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.register.username = this.registerForm.get('username').value;
+    this.register.email = this.registerForm.get('email').value;
+    this.register.password = this.registerForm.get('password').value;
+    this.register.confirmPassword = this.registerForm.get('confirmPassword').value;
+
+    this.authService.register(this.register).subscribe(data => {
+      console.log('register succes');
+      this.router.navigateByUrl('/register-success');
+    }, error => {
+      console.log('register failed');
+    });
+  }
 }
